5ec34056417bc576a7a509bae59b714c9c53bc4c
carly <carly@noukod.com>
2019-02-28 19:35:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file.
+// Code your solution here:
+function driversWithRevenueOver(drivers, revenue){
+    return drivers.filter(function (driver) {
+        return driver.revenue > revenue;
+    });
+}
+
+function driverNamesWithRevenueOver(drivers, revenue){
+    return driversWithRevenueOver(drivers,revenue).map(driver => driver.name);
+}
+
+function exactMatch(drivers, objectSpec){
+    return drivers.filter(function (driver) {
+        return driver[Object.keys(objectSpec)[0]] == Object.values(objectSpec)[0];
+    });
+}
+
+function exactMatchToList(drivers, objectSpec){
+    return exactMatch(drivers,objectSpec).map(driver => driver.name);
+}
